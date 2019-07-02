c3e0a1801797c1d6fdf4b2aa8835c24b7929a591
Anton Wieslander <antonwieslander@hotmail.com>
2019-07-02 18:15:33 +0100

    video upload

---
+++ HowToVideoFiles/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xabe.FFmpeg;
+using Xabe.FFmpeg.Enums;
+
+namespace HowToVideoFiles.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string _dir;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            _dir = env.WebRootPath;
+        }
+
+        [HttpGet]
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            using (var fileStream =
+                new FileStream(Path.Combine(_dir, "file.mp4"), FileMode.Create, FileAccess.Write))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            await ConvertVideo();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<bool> ConvertVideo()
+        {
+            try
+            {
+                var input = Path.Combine(_dir, "file.mp4");
+                var output = Path.Combine(_dir, "converted.mp4");
+
+                FFmpeg.ExecutablesPath = Path.Combine(_dir, "ffmpeg");
+
+                var info = await MediaInfo.Get(input);
+
+                var videoStream = info.VideoStreams.First()
+                    .SetCodec(VideoCodec.H264)
+                    .SetSize(VideoSize.Hd480);
+
+                await Conversion.New()
+                    .AddStream(videoStream)
+                    .SetOutput(output)
+                    .Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ HowToVideoFiles/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HowToVideoFiles
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
